8d5876641ef75d59825b3549d3e34a4234be46e0
Kyamal Khaidarov <khaidarov.kyamal@mail.ru>
2021-10-29 23:39:37 +0300

    jstl_1

--- src/main/java/ru/kpfu/itis/services/ProductsService.java
+++ src/main/java/ru/kpfu/itis/services/ProductsService.java
@@ -8,4 +8,11 @@ import java.util.List;
 public interface ProductsService {
     void add(ProductForm productForm);
     List<Product> findAll();
+
+    public void addToBucket(Long userId, Long productId);
+    public void addToFavourites(Long userId, Long productId);
+    List<Product> findFavouriteProductsByUserId(Long userId);
+    List<Product> findInBucketByUserId(Long userId);
+
+
 }
--- src/main/java/ru/kpfu/itis/servlets/ProductsServlet.java
+++ src/main/java/ru/kpfu/itis/servlets/ProductsServlet.java
@@ -3,14 +3,14 @@ package ru.kpfu.itis.servlets;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.kpfu.itis.forms.ProductForm;
 import ru.kpfu.itis.models.Product;
-import ru.kpfu.itis.repositories.ProductsRepository;
-import ru.kpfu.itis.repositories.ProductsRepositoryImpl;
-import ru.kpfu.itis.services.ProductsService;
-import ru.kpfu.itis.services.ProductsServiceImpl;
+import ru.kpfu.itis.models.User;
+import ru.kpfu.itis.repositories.*;
+import ru.kpfu.itis.services.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +23,7 @@ import java.util.List;
 @WebServlet("/products")
 public class ProductsServlet extends HttpServlet {
 
+    private UsersService usersService;
     private ProductsService productsService;
 
     private final String URL = "jdbc:postgresql://localhost:5432/test_project";
@@ -37,6 +38,9 @@ public class ProductsServlet extends HttpServlet {
 
             ProductsRepository productsRepository = new ProductsRepositoryImpl(connection);
             productsService = new ProductsServiceImpl(productsRepository);
+            UsersRepository usersRepository = new UsersRepositoryImpl(connection);
+            AuthRepository authRepository = new AuthRepostoryImpl(connection);
+            usersService = new UsersServicesImpl(usersRepository, authRepository);
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Unavailable");
             throw new UnavailableException("Сайт недоступен!!!");
@@ -57,11 +61,31 @@ public class ProductsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
 
+        Cookie cookie = CookieService.getCookie(req, "auth");
+        if (cookie != null) {
+            User user = usersService.findUserByCookieValue(cookie.getValue());
+            if (user != null) {
+                String addToBucketId = req.getParameter("to_bucket");
+                if (addToBucketId != null) {
+                    productsService.addToBucket(user.getId(), Long.valueOf(addToBucketId));
+                    req.getRequestDispatcher("/jsp/products.jsp").forward(req, resp);
+                }
+
+                String addToFavouriteId = req.getParameter("to_favourite");
+                if (addToFavouriteId != null) {
+                    productsService.addToFavourites(user.getId(), Long.valueOf(addToFavouriteId));
+                    req.getRequestDispatcher("/jsp/products.jsp").forward(req, resp);
+                }
+            }
+        }
+
         ObjectMapper objectMapper = new ObjectMapper();
 
         ProductForm productForm = objectMapper.readValue(req.getParameter("product"), ProductForm.class);
 
         productsService.add(productForm);
+
+        req.getRequestDispatcher("/jsp/products.jsp").forward(req, resp);
     }
 
 }

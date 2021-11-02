74dee249b33dbf783af0ed0dae92226ab9db2817
Elnur Sardarov <sardarovelnur@gmail.com>
2021-11-02 17:17:55 +0500

    put jsp folder into WEB-INF

--- src/main/java/ru/kpfu/itis/filters/BucketFilter.java
+++ src/main/java/ru/kpfu/itis/filters/BucketFilter.java
@@ -1,11 +1,13 @@
 package ru.kpfu.itis.filters;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebFilter("/bucket")
 public class BucketFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
